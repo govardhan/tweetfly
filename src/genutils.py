bcabf0ebdd1877c4bffa65c09ca46a5647aff32c
UV Admin <uvadmin@pluto.(none)>
2012-05-31 14:30:41 +0800

    App initialization done

--- src/genutils.py
+++ src/genutils.py
@@ -1,5 +1,6 @@
 
 import os,sys,io,logging
+import traceback
 import ConfigParser
 from patterns import singleton
 
@@ -9,7 +10,9 @@ class UVEnvi:
 
 @singleton
 class UVConf:
-  def __inint__(self, app_logger, app_conf_filename):
+  uvconf = None
+
+  def init(self, app_logger, app_conf_filename):
     #try:
     #  uv_home_path = os.environ['UV_HOME']
     #except KeyError:
@@ -17,19 +20,52 @@ class UVConf:
 
     try:
       uvconf = ConfigParser.RawConfigParser()
-      uvconfig.read(app_conf_filename)
-    except:
-      sys.stderr.write("UVConf initialization failed")
-      sys.exit(1)
-    
+      uvconf.read(app_conf_filename)
+      app_logger.info("UVConf initialization done for conf file %s.", app_conf_filename)
+      app_logger.info("uvconf sections are ")
 
-def init_log(app_name, log_filename, log_level=logging.DEBUG):
-  logger = logging.getLogger(app_name)
-  hdlr = logging.FileHandler(log_filename)  
-  formatter = logging.Formatter('%(asctime)s|%(process)-5d|%(thread)d|%(name)s|%(filename)s|%(func)s|%(levelname)s|%(message)s')
-  hdlr.setFormatter(formatter)
-  logger.addHandler(hdlr)
-  logger.setLevel(log_level)
-  return logger
-  
-  
+      for sec in uvconf.sections():
+        app_logger.info(("%s"% sec).center(60,'*'))
+        #app_logger.info("***************** %25s *************** ", sec)
+        for option in uvconf.options(sec):
+          app_logger.info("%16s => %-16s ", option, uvconf.get(sec, option))
+
+    except:
+      app_logger.critical("UVConf initialization failed for conf file %s. Terminating now. Traceback ...", app_conf_filename)
+      app_logger.exception(traceback.print_exc())
+      sys.exit(1)
+
+
+  def get(p_key):
+    return uvconf.get("core", p_key)
+
+  def get(p_section, p_key):
+    return uvconf.get(p_section, p_key)
+
+  def getint(p_section, p_key):
+    return uvconf.getint(p_section, p_key)
+
+  def getfloat(p_section, p_key):
+    return uvconf.getfloat(p_section, p_key)
+
+  def getboolean(p_section, p_key):
+    return uvconf.getboolean(p_section, p_key)
+
+
+def init_logger(app_name, log_filename, log_level=logging.DEBUG):
+  try:
+    logger = logging.getLogger(app_name)
+    hdlr = logging.FileHandler(log_filename)  
+    formatter = logging.Formatter('%(asctime)s|%(process)-5d|%(thread)d|%(filename)-16s|%(funcName)-16s|%(lineno)4d|%(levelname)-8s| %(message)s')
+    #formatter = logging.Formatter('%(asctime)s|%(process)-5d|%(thread)d|%(name)s|%(filename)-14s|%(funcName)s|%(lineno)4d|%(levelname)-7s|%(message)s')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+    logger.setLevel(log_level)
+    return logger
+  except:
+    sys.stderr.write("logger initialization failed. Terminating application now. Traceback ...")
+    sys.stderr.write(traceback.print_exc())
+    sys.stderr.flush()
+    sys.exit(1)
+
+
--- src/tweetflyapp.py
+++ src/tweetflyapp.py
@@ -1,3 +1,4 @@
+import sys,os
 import logging
 from patterns import singleton
 from genutils import UVConf
@@ -11,12 +12,26 @@ class TweetFlyApp:
   log_level = logging.DEBUG
   app_conf = None
 
-  def __init__(self, p_conf_filename, p_log_filename, p_log_level):
-    #TODO check log_filename and its path existance
+  def init(self, p_conf_filename, p_log_filename, p_log_level):
     #TODO validate log_level
+    self.app_logger = genutils.init_logger(__name__, p_log_filename, p_log_level);
+    self.app_logger.info("TweetFlyApp logger initialized" )
+    self.app_logger.info("p_conf_filename=%s p_log_filename=%s p_log_level=%s" ,p_conf_filename, p_log_filename ,str(p_log_level)  )
+    #check log_filename and its path existance
+    if ( False == os.path.exists(p_conf_filename) ):
+      self.app_logger.critical("Configuration file %s does not exists. Terminating application now", p_conf_filename)
+      sys.exit(1)
+
+    if ( False == os.path.exists(os.path.dirname(p_log_filename)) ):
+      self.app_logger.warning("Log directory %s does not exists. Creating new directory", os.path.dirname(p_log_filename))
+      os.makedirs(os.path.dirname(p_log_filename))
+      
+      
     conf_filename = p_conf_filename
-    app_logger = init_logger(__name__, p_log_filename, p_log_level);
-    app_conf = UVConf(app_logger, conf_filename);
+    self.app_logger.info("Initializing TweetFlyApp with configuration file %s", conf_filename )
+    self.app_conf = UVConf()
+    self.app_conf.init(self.app_logger, conf_filename);
+    self.app_logger.info("TweetFlyApp configuration initialization done")
 
 
   def start(self):
@@ -24,7 +39,7 @@ class TweetFlyApp:
     #TODO Authenticate
     #TODO Wait for updates
     for i in range(10):
-      app_logger.info("I am running now - %d" % i)
+      self.app_logger.info("I am running now - %d" % i)
     pass
 
   def on_tweet_update(self):
@@ -32,22 +47,23 @@ class TweetFlyApp:
     pass
   
 
+def main(argv):
+  #Looks like no additional configuration parameters
+  l_conf_filename = '/usr/local/uv/conf/tweetfly.conf'
+  l_log_filename = "/usr/local/uv/log/tweetfly.log"
+  l_log_level = logging.DEBUG
+  sys.stderr.write("l_conf_filename="+ l_conf_filename + " l_log_filename=" + l_log_filename + " l_log_level="+str(l_log_level)+ "\n")
+  sys.stderr.flush()
+    
+  tweet_fly_app = TweetFlyApp()
+  tweet_fly_app.init(l_conf_filename, l_log_filename, l_log_level)
+  tweet_fly_app.start()
+  
 
 if __name__ == '__main__':
   """
     Parameters - appname, conf_filename, log_filename, log_level
-    logging.DEBUG
-    10
-    logging.INFO
-    20
-    logging.WARNING
-    30
-    logging.ERROR
-    40
-    logging.CRITICAL
-    50
-
   """
-  #TODO Validate arguments
-  tweet_fly_app = TweetFlyApp(argv[1], argv[2], argv[3])
-  tweet_fly_app.start()
+  sys.stderr.write("Command line arguments "+ str(sys.argv)+ "\n")
+  sys.stderr.flush()
+  main(sys.argv)
