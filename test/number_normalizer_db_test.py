0dcd8a4f07a71bd86f8cb5d68e100be1fb5aa393
surya <suryareddy>
2012-06-03 18:52:20 +0800

    written test cases regarding db connections for number normalizer

---
+++ test/number_normalizer_db_test.py
@@ -0,0 +1,44 @@
+import unittest
+import mysqldb
+from number_normalizer import load_rules_from_db
+#checking if connection is established
+class nrmtest1(unittest.TestCase):
+  def runTest(self):
+    db = DBCon().get_connection(logger)
+    self.assertTrue(db)
+#checking if it is exiting if there are no rows
+class nrmtest2(unittest.TestCase):
+  def runTest(self):
+    c_query="CREATE TABLE mytable SELECT * FROM tbl_number_normalizer limit 10"
+    t_query="truncate table mytable"     
+    l_query = "SELECT id, in_pattern, out_pattern, telco_id, channel, description FROM mytable ORDER BY id DESC"
+    self.assertRaises(SystemExit)
+    d_query="drop table mytable"
+#checking for cursor 
+class nrmtest3(unittest.TestCase):
+  def runTest(self):
+    cursor=cursor = db.cursor()
+    self.assertTrue(cursor)
+#checking if logger has arguments
+class nrmtest4(unittest.TestCase):
+  def runTest(self):
+    logger = none
+    self.assertRaises(SystemExit)
+#adding a column
+class nrmtest5(unittest.TestCase):
+  def runTest(self):
+    c_query="CREATE TABLE mytable SELECT * FROM tbl_number_normalizer limit 10"
+    u_query="alter table mytable add pattern = int(10)"
+    self.assertTrue(logger.info)
+     d_query="drop table mytable"
+#deleting a column
+class nrmtest6(unittest.TestCase):
+  def runTest(self):
+    c_query="CREATE TABLE mytable SELECT * FROM tbl_number_normalizer limit 10"
+    u_query="alter table mytable drop in_pattern"
+    self.assertTrue(logger.info)
+     d_query="drop table mytable"
+
+
+
+
