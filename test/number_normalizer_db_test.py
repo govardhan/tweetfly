c77662d90fa5343f271ed22d6c3016b30d956f50
surya <suryareddy>
2012-06-03 19:42:38 +0800

    made some changes in db_test file.

--- test/number_normalizer_db_test.py
+++ test/number_normalizer_db_test.py
@@ -1,11 +1,14 @@
 import unittest
-import mysqldb
-from number_normalizer import load_rules_from_db
+import MySQLdb
+import sys
+sys.path.append('/home/surya/myprgms/tweetfly/src')
+from number_normalizer import NumNormalizer
 #checking if connection is established
 class nrmtest1(unittest.TestCase):
   def runTest(self):
     db = DBCon().get_connection(logger)
     self.assertTrue(db)
+
 #checking if it is exiting if there are no rows
 class nrmtest2(unittest.TestCase):
   def runTest(self):
@@ -30,14 +33,16 @@ class nrmtest5(unittest.TestCase):
     c_query="CREATE TABLE mytable SELECT * FROM tbl_number_normalizer limit 10"
     u_query="alter table mytable add pattern = int(10)"
     self.assertTrue(logger.info)
-     d_query="drop table mytable"
+    d_query="drop table mytable"
 #deleting a column
 class nrmtest6(unittest.TestCase):
   def runTest(self):
     c_query="CREATE TABLE mytable SELECT * FROM tbl_number_normalizer limit 10"
     u_query="alter table mytable drop in_pattern"
     self.assertTrue(logger.info)
-     d_query="drop table mytable"
+    d_query="drop table mytable"
+if __name__ == '__main__':
+  unittest.main()
 
 
 
